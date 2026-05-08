041cfe0fc7cf1abde80b4ccb3386107045023331
Tushar Tibude <tushar.tibude1000@gmail.com>
2026-05-08 19:22:48 +0530

    Add bump allocator free mechanism

--- kernel64/memory/bump.c
+++ kernel64/memory/bump.c
@@ -2,11 +2,20 @@
 #include <kernel64/memory/bump.h>
 #include <stddef.h>
 
+/* Free pages are used as linked list objects in the free list
+ * to create a list of free pages that can be reallocated on demand */
+struct bump_free_page {
+    struct bump_free_page* next;
+};
+
 struct bump_state {
     uint64_t bottom;
     uint64_t top;
     uint64_t allocation_pointer;
+
+    struct bump_free_page* free_list;
 };
+
 
 struct bump_state state;
 
@@ -14,6 +23,7 @@ void bump_initialize(uint64_t bottom, uint64_t top) {
     state.bottom = bottom;
     state.top = top;
     state.allocation_pointer = bottom;
+    state.free_list = NULL;
 
     if (!CHECK_PAGE_4K_ALIGN(state.bottom))
         state.bottom = PAGE_4K_ALIGN(state.bottom);
@@ -28,6 +38,15 @@ void* bump_allocate_page() {
     if (state.allocation_pointer == 0)
         return NULL;
 
+    /* Reuse page from freelist when possible */
+    if (state.free_list != NULL) {
+        struct bump_free_page* new_head = state.free_list->next;
+        void* allocated = state.free_list;
+        state.free_list = new_head;
+
+        return allocated;
+    }
+
     if (state.allocation_pointer + PAGE_4K >= state.top)
         return NULL;
 
@@ -37,7 +56,12 @@ void* bump_allocate_page() {
     return object;
 }
 
-/* TODO */
+/* Freeing adds the page to the free list, ready for reuse
+ * This creates fragmentation quickly but is alright for early bootstrapping */
 void bump_free_page(void* page) {
+    struct bump_free_page* head = state.free_list;
+    struct bump_free_page* new = (struct bump_free_page*)page;
 
+    new->next = head;
+    state.free_list = new;
 }
